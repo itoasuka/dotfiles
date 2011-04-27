eb61f3dc68d21b067370d7d9ac799d37415c8274
itoasuka <itoasuka@gmail.com>
2011-04-27 12:11:41 +0900

    セットアップ用のシェルスクリプト追加

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ln -s $HOME/dotfiles/.dircolors $HOME/.dircolors
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
+ln -s $HOME/dotfiles/.gitmodules $HOME/.gitmodules
+ln -s $HOME/dotfiles/.vim $HOME/.vim
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.git-completion.bash $HOME/.git-completion.bash
