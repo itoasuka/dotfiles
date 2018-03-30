ef426f04683791a6d7c4f0105549eddef73d9028
itoasuka <itoasuka@gmail.com>
2018-03-30 17:13:14 +0900

    fish shell 対応

--- setup.sh
+++ setup.sh
@@ -5,20 +5,29 @@ git submodule update
 
 cd $HOME
 
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+#git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-if [ -e $HOME/.zshrc ]; then
-  rm $HOME/.zshrc
-fi
+#if [ -e $HOME/.zshrc ]; then
+#  rm $HOME/.zshrc
+#fi
+#ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
+#ln -sf $HOME/dotfiles/.zprofile $HOME/.zprofile
+#ln -sf $HOME/dotfiles/.zpreztorc $HOME/.zpreztorc
+#ls -sf "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zlogin" $HOME/.zlogin
+#ls -sf "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zlogout" $HOME/.zlogout
+#ls -sf "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zshenv" $HOME/.zshenv
+
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+fisher install omf/theme-bobthefish
+fiher install z
+fisher install 0rax/fish-bd
+fisher oh-my-fish/plugin-peco
+mkdir -p $HOME/.config/fish
+ln -sf $HOME/dotfiles/.config/fish/config.fish $HOME/.config/fish/config.fish
+
 if [ ! -e $HOME/.config/nvim ]; then
   mkdir -p $HOME/.config/nvim
 fi
-ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -sf $HOME/dotfiles/.zprofile $HOME/.zprofile
-ln -sf $HOME/dotfiles/.zpreztorc $HOME/.zpreztorc
-ls -sf "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zlogin" $HOME/.zlogin
-ls -sf "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zlogout" $HOME/.zlogout
-ls -sf "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zshenv" $HOME/.zshenv
 if [ `uname` = 'Darwin' ]; then
   ln -sf $HOME/dotfiles/.gitconfig_mac $HOME/.gitconfig
 else
